86b59d5c6cef17ae63a48e6d11280bb298527a2e
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2023-01-22 10:19:48 +0000

    Link with shared library in package root

---
+++ decoder/decorder.go
@@ -0,0 +1,35 @@
+package decoder
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/../rapidyenc
+#cgo LDFLAGS: -L${SRCDIR}/../ -lrapidyenc
+#include "rapidyenc.h"
+*/
+import "C"
+import (
+	"unsafe"
+)
+
+func init() {
+	C.rapidyenc_decode_init()
+}
+
+func Decode(src []byte) C.size_t {
+	return C.rapidyenc_decode(
+		unsafe.Pointer(&src[0]),
+		unsafe.Pointer(&src[0]),
+		C.size_t(len(src)),
+	)
+}
+
+func DecodeIndirect(src []byte, dst []byte) C.size_t {
+	if len(src) > len(dst) {
+		panic("dst is smaller than src")
+	}
+
+	return C.rapidyenc_decode(
+		unsafe.Pointer(&src[0]),
+		unsafe.Pointer(&dst[0]),
+		C.size_t(len(src)),
+	)
+}
---
+++ encoder/encoder.go
@@ -0,0 +1,44 @@
+package encoder
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/../rapidyenc
+#cgo LDFLAGS: -L${SRCDIR}/../ -lrapidyenc
+#include "rapidyenc.h"
+*/
+import "C"
+import (
+	"unsafe"
+)
+
+func init() {
+	C.rapidyenc_encode_init()
+}
+
+func MaxLength(length int, line_size int) uint {
+	return uint(C.rapidyenc_encode_max_length(C.size_t(length), C.int(line_size)))
+}
+
+type Encoder struct {
+	LineLength int
+}
+
+func NewEncoder() *Encoder {
+	return &Encoder{
+		LineLength: 128,
+	}
+}
+
+func (e *Encoder) Encode(src []byte) ([]byte, error) {
+	// RAPIDYENC_API size_t rapidyenc_encode(const void* __restrict src, void* __restrict dest, size_t src_length);
+	// RAPIDYENC_API size_t rapidyenc_encode_ex(int line_size, int* column, const void* __restrict src, void* __restrict dest, size_t src_length, int is_end);
+
+	dst := make([]byte, MaxLength(len(src), e.LineLength))
+
+	encoded_size := C.rapidyenc_encode(
+		unsafe.Pointer(&src[0]),
+		unsafe.Pointer(&dst[0]),
+		C.size_t(len(src)),
+	)
+
+	return dst[:encoded_size], nil
+}
