dfb621a0bd09d29ad782313bf1743d489afb8aef
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2023-10-31 23:43:22 +0000

    Remove Windows building test script

--- windows.bat
+++
@@ -1,3 +0,0 @@
-cmake -S rapidyenc -B rapidyenc/build -DCMAKE_CXX_FLAGS=-msse4 -G "MinGW Makefiles"
-cmake --build rapidyenc/build --config Release
-copy "rapidyenc\build\rapidyenc_static\librapidyenc.a" "lib\librapidyenc_windows_amd64.a"
